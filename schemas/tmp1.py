9d275c10c2dd3c2bc684b14a09def17d715307b1
ARH <hakerti@gmail.com>
2023-03-27 19:57:06 +0200

    move to src

--- schemas/tmp1.py
+++
@@ -1,9 +0,0 @@
-import re
-import schemas.definitions.console_colors as cc
-
-scheme = [
-    { "pattern": re.compile(r"\n\b\ta([sS])da\d"), "replace": r"qwe \1 asd", "final": True },
-    { "pattern": re.compile(r"(\s)|(-)"), "replace": r", "},
-    { "pattern": re.compile(r"(Ema|Max)"), "replace": cc.format(r"\1", cc.Color.Red)},
-    { "pattern": re.compile(r"(Max)"), "replace": cc.format(r"\1", cc.Color.Blue), "final": True},
-]
